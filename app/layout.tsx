866b2ea2b09e5aae31f5da8b62178b1c6fd33765
pheany <114763753+pheany@users.noreply.github.com>
2023-06-12 16:48:12 +0100

    sonora layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
-import './globals.css'
-import { Figtree } from 'next/font/google'
+import Sidebar from '@/components/Sidebar';
+import './globals.css';
+import { Figtree } from 'next/font/google';
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Sonora',
-  description: 'Music app that lets you upload and listen to your favorite songs for free.',
-}
+  description:
+    'Music app that lets you upload and listen to your favorite songs for free.',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
-  )
+  );
 }
