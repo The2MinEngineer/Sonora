4698d762ba6834c2e36b14c90ed1d63401224248
pheany <114763753+pheany@users.noreply.github.com>
2023-06-19 02:00:01 +0100

    Authentication Modal and Functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css';
 import { Figtree } from 'next/font/google';
 import SupabaseProvider from '@/providers/SupabaseProvider';
 import UserProvider from '@/providers/userProvider';
+import ModalProvider from '@/providers/ModalProvider';
+import ToasterProvider from '@/providers/ToasterProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -20,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
