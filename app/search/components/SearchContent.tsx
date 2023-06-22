4fc31e3b179bac0c204264679340da26984b9d36
pheany <114763753+pheany@users.noreply.github.com>
2023-06-22 02:10:47 +0100

    Player Funtionality

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,25 +1,28 @@
 'use client';
 
-import LikeButton from '@/components/LikeButton';
-import MediaItem from '@/components/MediaItem';
 import { Song } from '@/types';
+import MediaItem from '@/components/MediaItem';
+import LikeButton from '@/components/LikeButton';
+import useOnPlay from '@/hooks/useOnPlay';
 
 interface SearchContentProps {
   songs: Song[];
 }
 
 const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return (
       <div
         className="
-            flex
-            flex-col
-            gap-y-2
-            w-full
-            py-6
-            text-neutral-400
-            "
+          flex 
+          flex-col 
+          gap-y-2 
+          w-full 
+          px-6 
+          text-neutral-400
+        "
       >
         No songs found.
       </div>
@@ -27,13 +30,13 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
   }
 
   return (
-    <div className="flex flex-col gap-y-2 w-full py-6">
-      {songs.map((song) => (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song: Song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
-            <LikeButton songId={song.id} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
